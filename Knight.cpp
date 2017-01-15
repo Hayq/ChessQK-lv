019a70b0c5b3d5a9de7b81d50d860fb997ae4581
Hayq <hayq010196@gmail.com>
2017-01-15 18:13:58 +0400

    Update Knight.cpp

--- Knight.cpp
+++ Knight.cpp
@@ -137,7 +137,7 @@ void Knight::Show()
 Knight :: ~Knight()
 {
 	cout << "Knight Desturctor" << endl;
-	for(int i = 0 ; i < moves.size() - 1; ++i)
+	for(int i = 0 ; i < moves.size(); ++i)
 	{
 		position = moves[i];
 		delete position;
